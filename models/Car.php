c9b705dc7df5393a51aa3d047cfa76e8ab024302
Rockman1233 <rocki67@me.com>
2017-10-31 14:16:55 +0300

    Car table

--- models/Car.php
+++ models/Car.php
@@ -44,11 +44,8 @@ class Car extends Object {
     }
 
     public function safeCar() {
-        (isset($this->id) ? $this->db->query('UPDATE Car SET state_num='.$this->state_num.',mileage='.$this->mileage.',int_of_availability='.$this->int_of_avaliability.',
-        cost_less_30_inc='.$this->cost_less_30.',cost_more_31='.$this->cost_more_31.',car_owner='.$this->car_owner):
-            //else
 
-            $this->db->query("INSERT INTO 
+        $asd = "INSERT INTO 
             Car(mark,
             model,
             year,
@@ -56,21 +53,27 @@ class Car extends Object {
             mileage,
             colour,
             consumption,
-            int_of_availability,
             cost_less_30_inc,
             cost_more_31) 
             VALUES 
-            ($this->mark, 
-            $this->model, 
+            ('$this->mark', 
+            '$this->model', 
             $this->year, 
             $this->state_num, 
             $this->mileage, 
-            $this->colour, 
+            '$this->colour', 
             $this->consumption,
-            $this->int_of_availability, 
             $this->cost_less_30, 
-            $this->cost_more_31)"));
+            $this->cost_more_31)";
+        $asd = str_replace('"', "", $asd);
+        var_dump($asd);
 
+        (isset($this->id) ? $this->db->query('UPDATE Car SET state_num='.$this->state_num.',mileage='.$this->mileage.',int_of_availability='.$this->int_of_avaliability.',
+        cost_less_30_inc='.$this->cost_less_30.',cost_more_31='.$this->cost_more_31.',car_owner='.$this->car_owner):
+            //else
+
+            $this->db->query($asd)
+        );
 
 
         //если мы добавляем новую машину в бд то используем все поля а если уже существующую то не все (например цвет или марка остаются прежними)
