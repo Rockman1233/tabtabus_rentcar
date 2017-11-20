0dd4ff9b689aae91c081330bf592b3516362eec5
Rockman1233 <rocki67@me.com>
2017-11-20 15:01:30 +0300

    del routes

--- config/routes.php
+++ config/routes.php
@@ -8,13 +8,11 @@
 
 return array(
 
-    'news/([a-z]+)/([0-9]+)' => 'news/view/$1/$2',
     'car' => 'car/index',
     'car/contract' => 'cabinet/addcar',
     'owner' => 'owner/index',
     'driver' => 'driver/index',
     'user' => 'user/index',
     'cabinet' => 'cabinet/index',
-    "^products$" => "product/view",
     '' => '',
 );
